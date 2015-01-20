453cca000a42d3f0e2ca9f7887faf2f643b69928
Niklas <ek.niklas@gmail.com>
2015-01-20 12:17:40 +0100

    Update to work with lingon 2.*

--- index.js
+++ index.js
@@ -4,16 +4,18 @@ var gulp = require('gulp');                 // for gulp.watch
 
 module.exports = function(lingon, config) {
   if(!config) config = {};
-  config.watchDir = config.watchDir || process.cwd() + '/' + lingon.sourcePath;
+  config.watchDir = config.watchDir || process.cwd() + '/' + lingon.config.sourcePath;
   config.watchDir += "/**/*";
   console.log(config.watchDir);
 
-  var watchFn = function() {
+  var watchFn = function(callback) {
     gulp.task('default', function(){
       lingon.log("Go!");
       gulp.watch(config.watchDir, function(){
         lingon.log("Build triggered by watch");
-        lingon.build();
+        lingon.build({
+          'callback': callback
+        });
       });
     });
     gulp.start();
@@ -22,6 +24,4 @@ module.exports = function(lingon, config) {
   lingon.registerTask('watch', watchFn, {
     message: "Lingon watching " + config.watchDir + "."
   });
-
 };
-
